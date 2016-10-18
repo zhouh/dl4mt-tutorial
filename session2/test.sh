dbe5a62640550a52ff65df393e812caa0ba2a7f6
zhouh <haozhou0806@gmail.com>
2016-10-18 16:28:24 +0800

    Temp commit.

--- session2/test.sh
+++ session2/test.sh
@@ -7,12 +7,12 @@
 
 export THEANO_FLAGS=device=cpu,floatX=float32
 
-cd $PBS_O_WORKDIR
+#cd $PBS_O_WORKDIR
 python ./translate.py -n -p 8 \
-	$HOME/models/model_session2.npz  \
-	$HOME/data/europarl-v7.fr-en.en.tok.pkl \
-	$HOME/data/europarl-v7.fr-en.fr.tok.pkl \
-	$HOME/data/newstest2011.en.tok \
+	$HOME/dl4mt-tutorial-master/session2/model_hal.npz  \
+	$HOME/dl4mt-tutorial-master/data/europarl-v7.fr-en.en.tok.pkl \
+	$HOME/dl4mt-tutorial-master/data/europarl-v7.fr-en.fr.tok.pkl \
+	$HOME/dl4mt-tutorial-master/data/newstest2011.en.tok \
 	./newstest2011.trans.fr.tok
 
 
