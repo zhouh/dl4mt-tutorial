72806349730fbd96bb1b592140166f160954932a
zhouh <haozhou0806@gmail.com>
2016-12-19 11:52:57 +0800

    Add the validation shell for computing bleu.

--- chunkNMT/translate_gpu.py
+++ chunkNMT/translate_gpu.py
@@ -13,8 +13,106 @@ from nmt import (build_sampler, gen_sample, load_params,
 from multiprocessing import Process, Queue
 
 
-def main(model, pklmodel, dictionary, dictionary_target, source_file, saveto, ck=5, wk=5,k=8,
-         normalize=False, n_process=5, chr_level=False):
+
+def translate_file(options, model, word_dict, word_idict_trg, source_file, saveto, ck=20, wk=3,k=8, normalize=True, n_process=5, chr_level=False):
+
+    # utility function
+    def _seqs2words(caps):
+        capsw = []
+        for cc in caps:
+            ww = []
+            for w in cc:
+                if w == 0:
+                    continue
+                # if w == -10000:
+                #     ww.append('| NOTEND')
+                #     continue
+                elif w < 0:
+                    # ww.append('|' +  str(w))
+                    continue
+                ww.append(word_idict_trg[w])
+            capsw.append(' '.join(ww))
+        return capsw
+
+    def _send_jobs(fname):
+        retval = []
+        with open(fname, 'r') as f:
+            for idx, line in enumerate(f):
+                if chr_level:
+                    words = list(line.decode('utf-8').strip())
+                else:
+                    words = line.strip().split()
+                x = map(lambda w: word_dict[w] if w in word_dict else 1, words)
+                x = map(lambda ii: ii if ii < options['n_words_src'] else 1, x)
+                x += [0]
+                retval.append(x)
+        return retval
+
+
+    print 'Translating ', source_file, '...'
+
+    print 'look up table'
+    n_samples = _send_jobs(source_file)
+
+    from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+    trng = RandomStreams(1234)
+    use_noise = theano.shared(numpy.float32(0.))
+
+    # allocate model parameters
+    params = init_params(options)
+
+    # load model parameters and set theano shared variables
+    params = load_params(model, params)
+    tparams = init_tparams(params)
+
+    # word index
+    f_init, f_next_chunk, f_next_word = build_sampler(tparams, options, trng, use_noise)
+
+
+
+    def _translate(seq):
+
+        be_stochastic = False
+        # sample given an input sequence and obtain scores
+        sample, score = gen_sample(tparams, f_init, f_next_chunk, f_next_word,
+                                   numpy.array(seq).reshape([len(seq), 1]),
+                                   options, trng=trng, maxlen_words=5, k_chunk=ck, k_word=wk, k=k,
+               maxlen_chunks=50,
+                                   stochastic=be_stochastic, argmax=True)
+
+        if be_stochastic:
+            return sample
+
+        # normalize scores according to sequence lengths
+        if normalize:
+            lengths = numpy.array([len(s) for s in sample])
+            score = score / lengths
+
+        # print 'score', score
+        # print 'candidates', sample
+
+        sidx = numpy.argmin(score)
+        return sample[sidx]
+
+
+    ys = []
+    idx = 0
+    for x in n_samples:
+
+        y = _translate(x)
+        ys.append(y)
+        print idx
+        idx += 1
+
+    trans = _seqs2words(ys)
+
+    with open(saveto, 'w') as f:
+        print >>f, '\n'.join(trans)
+    print 'Done'
+
+
+def main(model, pklmodel, dictionary, dictionary_target, source_file, saveto, ck=12, wk=3,k=8,
+         normalize=True, n_process=5, chr_level=False):
 
     print 'load model model_options'
     with open('%s' % pklmodel, 'rb') as f:
@@ -135,8 +233,8 @@ def main(model, pklmodel, dictionary, dictionary_target, source_file, saveto, ck
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ck', type=int, default=3)
-    parser.add_argument('-wk', type=int, default=5)
+    parser.add_argument('-ck', type=int, default=12)
+    parser.add_argument('-wk', type=int, default=3)
     parser.add_argument('-k', type=int, default=8)
     parser.add_argument('-p', type=int, default=5)
     parser.add_argument('-n', action="store_true", default=False)
---
+++ chunkNMT/validate.py
@@ -0,0 +1,245 @@
+'''
+Translates a source file using a translation model.
+'''
+import argparse
+
+import numpy
+import theano
+import cPickle as pkl
+import heapq
+from translate_gpu import translate_file
+import logging
+import time
+import subprocess
+import re
+
+from nmt import (build_model, pred_probs, load_params,
+                 init_params, init_tparams, prepare_training_data)
+
+from training_data_iterator import TrainingTextIterator
+
+def getCost(tparams, options, model, valid):
+
+
+
+    trng, use_noise, \
+    x, x_mask, y_chunk, y_chunk_mask, y_cw, y_cw_mask, lw_in_chunk, \
+    opt_ret, \
+    cost= \
+        build_model(tparams, options)
+
+
+    inps = [x, x_mask, y_chunk, y_chunk_mask, y_cw, y_cw_mask, lw_in_chunk]
+
+
+
+    # before any regularizer
+    print 'Building f_log_probs...',
+    f_log_probs = theano.function(inps, cost, profile=False)
+    print 'Done'
+
+    valid_errs = pred_probs(f_log_probs, prepare_training_data,
+                                        options, valid)
+    valid_err = valid_errs.mean()
+    # allocate model parameters
+    params = init_params(options)
+
+    # load model parameters and set theano shared variables
+    params = load_params(model, params)
+    tparams = init_tparams(params)
+
+
+    trng, use_noise, \
+    x, x_mask, y_chunk, y_chunk_mask, y_cw, y_cw_mask, lw_in_chunk, \
+    opt_ret, \
+    cost= \
+        build_model(tparams, options)
+
+
+    inps = [x, x_mask, y_chunk, y_chunk_mask, y_cw, y_cw_mask, lw_in_chunk]
+
+
+
+    # before any regularizer
+    print 'Building f_log_probs...',
+    f_log_probs = theano.function(inps, cost, profile=False)
+    print 'Done'
+
+    valid_errs = pred_probs(f_log_probs, prepare_training_data,
+                                        options, valid)
+    valid_err = valid_errs.mean()
+
+    return valid_err
+
+def getBLEU():
+
+    return 0
+
+
+
+
+
+
+def main(model,
+         pklmodel,
+         logfile,
+         outputfile,
+         bleu_scrip,
+         valid_datasets=['../data/dev/newstest2011.en.tok',
+                          '../data/dev/newstest2011.fr.tok'],
+         dictionaries=[
+              '/data/lisatmp3/chokyun/europarl/europarl-v7.fr-en.en.tok.pkl',
+              '/data/lisatmp3/chokyun/europarl/europarl-v7.fr-en.fr.tok.pkl'],
+         dictionary_chunk='/data/lisatmp3/chokyun/europarl/europarl-v7.fr-en.en.tok.pkl',
+         beginModelIter=200000,
+         k_best_keep=10):
+
+
+
+
+    f = open(logfile, 'w')
+
+    best_bleu = -1
+    best_bleu_iter = beginModelIter
+
+    heap = []
+    heapq.heapify(heap)
+
+    # load the dictionaries of both source and target
+    # load dictionaries and invert them
+    worddicts = [None] * len(dictionaries)
+    worddicts_r = [None] * len(dictionaries)
+    for ii, dd in enumerate(dictionaries):
+        with open(dd, 'rb') as f:
+            worddicts[ii] = pkl.load(f)
+        worddicts_r[ii] = dict()
+        for kk, vv in worddicts[ii].iteritems():
+            worddicts_r[ii][vv] = kk
+
+    # dict for chunk label
+    worddict_chunk = [None]
+    worddict_r_chunk = [None]
+    with open(dictionary_chunk, 'rb') as f:
+        worddict_chunk = pkl.load(f)
+    worddict_r_chunk = dict()
+    for kk, vv in worddict_chunk.iteritems():
+        worddict_r_chunk[vv] = kk
+    print worddict_chunk
+
+    print 'load model model_options'
+    with open('%s' % pklmodel, 'rb') as f:
+        options = pkl.load(f)
+
+
+
+    for iter in range(beginModelIter, 500000, 1000):
+
+
+        model_file_name = model + '.iter' + str(iter) + '.npz'
+
+
+        # build valid set
+        valid = TrainingTextIterator(valid_datasets[0], valid_datasets[1],
+                                     dictionaries[0], dictionaries[1], dictionary_chunk,
+                                     n_words_source=options['n_words_src'], n_words_target=options['n_words'],
+                                     batch_size=options['batch_size'],
+                                     max_chunk_len=options['maxlen_chunk'], max_word_len=options['maxlen_chunk_words'])
+
+
+        # allocate model parameters
+        params = init_params(options)
+
+        # load model parameters and set theano shared variables
+        params = load_params(model_file_name, params)
+        tparams = init_tparams(params)
+
+        currentIterCost = -1 * getCost(tparams, options, model_file_name, valid)
+
+        print >> f, '==========================='
+
+        print >> f, 'Iter ' + str(iter) + ', Cost' + str(-1 * currentIterCost)
+
+
+        compute_bleu = False
+
+        if len(heap) < k_best_keep:
+            compute_bleu = True
+        else:
+            top_item = heap[0] # smallest in heap
+            if top_item < currentIterCost: # min heap
+                heapq.heappop(heap)
+                compute_bleu = True
+
+
+        if compute_bleu:
+            heapq.heappush(heap, currentIterCost)
+            print >> f, '###Compute BLEU at Iter '+str(iter)
+
+
+
+            output_iter = outputfile + str(iter)
+
+            val_start_time = time.time()
+
+            print >>f, "Decoding took {} minutes".format(float(time.time() - val_start_time) / 60.)
+
+            translate_file(options,
+                           model_file_name,
+                           worddicts[0],
+                           worddicts_r[1],
+                           valid_datasets[0],
+                           output_iter)
+
+
+            cmd_bleu_cmd = ['perl', bleu_scrip, \
+                            valid_datasets[2], \
+                            '<', \
+                            output_iter, \
+                            '>'
+                            './output.eva']
+
+            subprocess.check_call(" ".join(cmd_bleu_cmd), shell=True)
+
+            fin = open('./output.eva', 'rU')
+            out = re.search('BLEU = [-.0-9]+', fin.readlines()[0])
+            fin.close()
+
+            bleu_score = float(out.group()[13:])
+
+            print >>f, 'Iter '+str(iter) + 'BLEU: ' + str(bleu_score)
+
+            if bleu_score > best_bleu:
+                best_bleu = bleu_score
+                best_bleu_iter = iter
+
+            print >>f, '## Best BLEU: ' + str(best_bleu) + 'at Iter' + str(best_bleu_iter)
+
+            f.flush()
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model', type=str)
+    parser.add_argument('pklmodel', type=str)
+    parser.add_argument('logfile', type=str)
+    parser.add_argument('outputfile', type=str)
+    parser.add_argument('bleu_scrip', type=str)
+    parser.add_argument('dictionary', type=str)
+    parser.add_argument('dictionary_target', type=str)
+    parser.add_argument('dictionary_chunk', type=str)
+    parser.add_argument('valid_source', type=str)
+    parser.add_argument('valid_target', type=str)
+    parser.add_argument('valid_reference', type=str)
+
+    args = parser.parse_args()
+
+    main(args.model,
+         args.pklmodel,
+         args.logfile,
+         args.outputfile,
+         args.bleu_scrip,
+         valid_datasets=[args.valid_source, args.valid_target, args.valid_reference],
+         dictionaries=[args.dictionary, args.dictionary_target],
+         dictionary_chunk=args.dictionary_chunk )
+
